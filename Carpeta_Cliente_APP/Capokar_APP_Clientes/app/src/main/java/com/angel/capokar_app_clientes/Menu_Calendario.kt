f3019d92919036b0f0c3c3ff252ceeeb1d0229a7
angelr10 <andreh10155@gmail.com>
2024-03-11 16:31:41 +0100

    Agregando el menu 01 calendar.xml, update 01 visual del xml

---
+++ Carpeta_Cliente_APP/Capokar_APP_Clientes/app/src/main/java/com/angel/capokar_app_clientes/Menu_Calendario.kt
@@ -0,0 +1,11 @@
+package com.angel.capokar_app_clientes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Calendario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.a01_activity_menu_calendario)
+    }
+}
